ea41b0e98c0bc0b38d9388f3cc363833f8e1a507
Ryan James <ryan@ryanjam.es>
2025-03-27 01:15:49 -0700

    Work scaffolding

--- src/data/works.ts
+++ src/data/works.ts
@@ -5,8 +5,8 @@ const works: TWorks = [
     category: "Product Design",
     items: [
       {
-        title: "Monorail",
-        slug: "monorail",
+        title: "Betteromics",
+        slug: "betteromics-product-design",
         description:
           "Donec sed odio dui. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas sed diam eget risus varius blandit sit amet non magna.",
         images: [
@@ -17,17 +17,111 @@ const works: TWorks = [
         ],
       },
       {
-        title: "Case Study 2",
-        slug: "case-study-2",
-        description: "Case Study 2 Description",
+        title: "Hourstack",
+        slug: "hourstack",
+        description:
+          "Donec sed odio dui. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas sed diam eget risus varius blandit sit amet non magna.",
         images: [
           {
             src: "/images/case-study-image.jpg",
             alt: "Case Study 2 Image 1",
           },
+        ],
+      },
+      {
+        title: "Innovationist",
+        slug: "Innovationist",
+        description:
+          "Donec sed odio dui. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas sed diam eget risus varius blandit sit amet non magna.",
+        images: [
           {
             src: "/images/case-study-image.jpg",
-            alt: "Case Study 2 Image 2",
+            alt: "Case Study 2 Image 1",
+          },
+        ],
+      },
+      {
+        title: "Diffleague",
+        slug: "diffleague",
+        description:
+          "Donec sed odio dui. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas sed diam eget risus varius blandit sit amet non magna.",
+        images: [
+          {
+            src: "/images/case-study-image.jpg",
+            alt: "Case Study 2 Image 1",
+          },
+        ],
+      },
+      {
+        title: "Luminator",
+        slug: "luminator",
+        description:
+          "Donec sed odio dui. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas sed diam eget risus varius blandit sit amet non magna.",
+        images: [
+          {
+            src: "/images/case-study-image.jpg",
+            alt: "Case Study 2 Image 1",
+          },
+        ],
+      },
+      {
+        title: "Desiring God",
+        slug: "desiring-god",
+        description:
+          "Donec sed odio dui. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas sed diam eget risus varius blandit sit amet non magna.",
+        images: [
+          {
+            src: "/images/case-study-image.jpg",
+            alt: "Case Study 2 Image 1",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    category: "Web Development",
+    items: [
+      {
+        title: "Betteromics",
+        slug: "betteromics-web-development",
+        description: "Case Study 3 Description",
+        images: [
+          {
+            src: "/images/case-study-image.jpg",
+            alt: "Case Study 3 Image 1",
+          },
+        ],
+      },
+      {
+        title: "Sanctus Music Library",
+        slug: "sanctus",
+        description: "Case Study 4 Description",
+        images: [
+          {
+            src: "/images/case-study-image.jpg",
+            alt: "Case Study 4 Image 1",
+          },
+        ],
+      },
+      {
+        title: "Bumbershoot",
+        slug: "bumbershoot",
+        description: "Case Study 4 Description",
+        images: [
+          {
+            src: "/images/case-study-image.jpg",
+            alt: "Case Study 4 Image 1",
+          },
+        ],
+      },
+      {
+        title: "Paper Crane Factory",
+        slug: "paper-crane-factory",
+        description: "Case Study 4 Description",
+        images: [
+          {
+            src: "/images/case-study-image.jpg",
+            alt: "Case Study 4 Image 1",
           },
         ],
       },
@@ -37,28 +131,205 @@ const works: TWorks = [
     category: "Branding",
     items: [
       {
-        title: "Case Study 3",
-        slug: "case-study-3",
+        title: "Figment",
+        slug: "figment",
         description: "Case Study 3 Description",
         images: [
           {
             src: "/images/case-study-image.jpg",
             alt: "Case Study 3 Image 1",
           },
+        ],
+      },
+      {
+        title: "The San Francisco Standard",
+        slug: "san-francisco-standard",
+        description: "Case Study 3 Description",
+        images: [
           {
             src: "/images/case-study-image.jpg",
-            alt: "Case Study 3 Image 2",
+            alt: "Case Study 3 Image 1",
           },
         ],
       },
       {
-        title: "Case Study 4",
-        slug: "case-study-4",
-        description: "Case Study 4 Description",
+        title: "Motherly",
+        slug: "motherly",
+        description: "Case Study 3 Description",
         images: [
           {
             src: "/images/case-study-image.jpg",
-            alt: "Case Study 4 Image 1",
+            alt: "Case Study 3 Image 1",
+          },
+        ],
+      },
+      {
+        title: "Chariot",
+        slug: "chariot",
+        description: "Case Study 3 Description",
+        images: [
+          {
+            src: "/images/case-study-image.jpg",
+            alt: "Case Study 3 Image 1",
+          },
+        ],
+      },
+      {
+        title: "Itel International",
+        slug: "itel",
+        description: "Case Study 3 Description",
+        images: [
+          {
+            src: "/images/case-study-image.jpg",
+            alt: "Case Study 3 Image 1",
+          },
+        ],
+      },
+      {
+        title: "nPower",
+        slug: "n-power",
+        description: "Case Study 3 Description",
+        images: [
+          {
+            src: "/images/case-study-image.jpg",
+            alt: "Case Study 3 Image 1",
+          },
+        ],
+      },
+      {
+        title: "Wave Neuro",
+        slug: "wave-neuro",
+        description: "Case Study 3 Description",
+        images: [
+          {
+            src: "/images/case-study-image.jpg",
+            alt: "Case Study 3 Image 1",
+          },
+        ],
+      },
+      {
+        title: "Monorail",
+        slug: "monorail",
+        description: "Case Study 3 Description",
+        images: [
+          {
+            src: "/images/case-study-image.jpg",
+            alt: "Case Study 3 Image 1",
+          },
+        ],
+      },
+      {
+        title: "Somn",
+        slug: "somn",
+        description: "Case Study 3 Description",
+        images: [
+          {
+            src: "/images/case-study-image.jpg",
+            alt: "Case Study 3 Image 1",
+          },
+        ],
+      },
+      {
+        title: "Wing It",
+        slug: "wing-it",
+        description: "Case Study 3 Description",
+        images: [
+          {
+            src: "/images/case-study-image.jpg",
+            alt: "Case Study 3 Image 1",
+          },
+        ],
+      },
+      {
+        title: "Holy Ground",
+        slug: "holy-ground",
+        description: "Case Study 3 Description",
+        images: [
+          {
+            src: "/images/case-study-image.jpg",
+            alt: "Case Study 3 Image 1",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    category: "Graphic Design",
+    items: [
+      {
+        title: "Sufjan Stevens",
+        slug: "sufjan-stevens",
+        description: "Case Study 3 Description",
+        images: [
+          {
+            src: "/images/case-study-image.jpg",
+            alt: "Case Study 3 Image 1",
+          },
+        ],
+      },
+      {
+        title: "The Classic Crime",
+        slug: "the-classic-crime",
+        description: "Case Study 3 Description",
+        images: [
+          {
+            src: "/images/case-study-image.jpg",
+            alt: "Case Study 3 Image 1",
+          },
+        ],
+      },
+      {
+        title: "RELEVANT Magazine",
+        slug: "relevant-magazine",
+        description: "Case Study 3 Description",
+        images: [
+          {
+            src: "/images/case-study-image.jpg",
+            alt: "Case Study 3 Image 1",
+          },
+        ],
+      },
+      {
+        title: "Lowercase Noises",
+        slug: "lowercase-noises",
+        description: "Case Study 3 Description",
+        images: [
+          {
+            src: "/images/case-study-image.jpg",
+            alt: "Case Study 3 Image 1",
+          },
+        ],
+      },
+      {
+        title: "Pacific Gold",
+        slug: "pacific-gold",
+        description: "Case Study 3 Description",
+        images: [
+          {
+            src: "/images/case-study-image.jpg",
+            alt: "Case Study 3 Image 1",
+          },
+        ],
+      },
+      {
+        title: "Jessica Gallo",
+        slug: "jessica-gallo",
+        description: "Case Study 3 Description",
+        images: [
+          {
+            src: "/images/case-study-image.jpg",
+            alt: "Case Study 3 Image 1",
+          },
+        ],
+      },
+      {
+        title: "Angelo De Augustine",
+        slug: "angelo-de-augustine",
+        description: "Case Study 3 Description",
+        images: [
+          {
+            src: "/images/case-study-image.jpg",
+            alt: "Case Study 3 Image 1",
           },
         ],
       },
