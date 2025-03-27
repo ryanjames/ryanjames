a667e79bf67c16183ce8f1edbe954b472a2a26bc
Ryan James <ryan@ryanjam.es>
2025-03-26 17:58:38 -0700

    Add Nacelle font

--- src/components/Styles.tsx
+++ src/components/Styles.tsx
@@ -1,6 +1,23 @@
-import { styled, createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
 
 export const SGlobal = createGlobalStyle`
+  @font-face {
+    font-family: "Nacelle";
+    src: url("/fonts/Nacelle-Regular.woff2") format("woff2"),
+         url("/fonts/Nacelle-Regular.woff") format("woff");
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: "Nacelle";
+    src: url("/fonts/Nacelle-Bold.woff2") format("woff2"),
+         url("/fonts/Nacelle-Bold.woff") format("woff");
+    font-weight: 700;
+    font-style: normal;
+  }
+
   * {
     margin: 0;
     padding: 0;
@@ -8,12 +25,8 @@ export const SGlobal = createGlobalStyle`
   }
 
   body {
-    font-family: 'Arial', sans-serif;
+    font-family: 'Nacelle', system-ui, -apple-system, "system-ui", "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
     background-color: #f5f5f5;
     color: #333;
   }
 `;
-
-export const SBody = styled.div`
-  padding-top: 50px;
-`
